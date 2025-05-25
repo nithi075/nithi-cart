2d7537f61cbf8255c0e347ea08e5356509ab88fe
nithi075 <nithishraj8055@gmail.com>
2025-05-25 15:42:41 +0530

    Fix vite.config.js to include index.html in build

--- frontend/nithi-app/vite.config.js
+++ frontend/nithi-app/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
@@ -10,10 +10,6 @@ export default defineConfig({
     jsxFragment: 'React.Fragment',
   },
   build: {
-    outDir: 'dist', // Ensures the output folder is correctly set
-    rollupOptions: {
-    input: 'src/main.jsx' // Ensures entry point is correct
-    }
+    outDir: 'dist', 
   }
 });
-
